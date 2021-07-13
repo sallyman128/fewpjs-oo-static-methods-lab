10ce7c3f3c3a16fd09d283e908e9bee91f41719b
Salmaan <salmaan.arif.ali@gmail.com>
2021-07-13 11:40:42 -0400

    commit tree f90a9446acb85223a553e9ac0539b55d60426bb0

--- index.js
+++ index.js
@@ -1,3 +1,28 @@
 class Formatter {
   //add static methods here
+
+  static capitalize(string) {
+    string = string.split("")
+    string[0] = string[0].toUpperCase()
+    return string.join("");
+  }
+
+  static sanitize(string) {
+    return string.replace(/[^A-Za-z0-9-' ]+/g, "")
+  }
+
+  static titleize(string) {
+    string = string.split(" "); //seperate title into an array of words
+    string[0] = Formatter.capitalize(string[0]) //capitalize the first word
+
+    const ignoreWords = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from']
+    for (let i=1; i < string.length; i++) {
+
+      ignoreWords.includes(string[i]) ? null : string[i] = Formatter.capitalize(string[i]);
+      // if ( ignoreWords.includes(string[i]) ) {
+      //   string[i] = Formatter.capitalize(string[i])
+      // }
+    }
+    return string.join(" ");
+  }
 }
